1642bd6fb40e7a4fe5bf7a96e1e522ed8db5dbad
pxw <08213064@cumt.edu.cn>
2023-09-18 20:42:13 +0800

    add etcd register

---
+++ byteview.go
@@ -0,0 +1,25 @@
+package simplecache
+
+//定义存储数据，get时返回slice的拷贝，防止修改底层数据
+
+type byteview struct {
+	b []byte
+}
+
+func cloneBytes(bytes []byte) []byte {
+	cloneByte := make([]byte, len(bytes))
+	copy(cloneByte, bytes)
+	return cloneByte
+}
+
+func (v *byteview) Len() int {
+	return len(v.b)
+}
+
+func (v *byteview) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+func (v *byteview) String() string {
+	return string(v.b)
+}
